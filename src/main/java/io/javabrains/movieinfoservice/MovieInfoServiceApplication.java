64631eb2b90e74d0a538c6974fdffbbf69847026
Cosmina Iacob <cosmina.iacob94@gmail.com>
2020-01-27 17:21:32 +0200

    #2 - Eureka

--- src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
+++ src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,12 @@ package io.javabrains.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+// this annotation is not mandatory anymore, as we added the dependency in the pom and it is enough
+//but we can add it to be more specific that this is an eureka client
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
